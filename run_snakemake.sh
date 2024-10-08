bacb441002f683a082246073a3baf3299c35c7a2
Anni Zheng <annizheng3@gmail.com>
2024-10-08 17:32:57 -0400

    Update run_snakemake.sh

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#BSUB -W 120:00                 # for 120 hours of wall clock time
-#BSUB -J run_snakemake          # Job title
-#BSUB -o run_snakemake.out   # Output file name
-#BSUB -e run_snakemake.err   # Error file name
+#SLURM -W 120:00                 # for 120 hours of wall clock time
+#SLURM -J run_snakemake          # Job title
+#SLURM -o run_snakemake.out   # Output file name
+#SLURM -e run_snakemake.err   # Error file name
 
 # activate snakemake mamba environment
 source $HOME/.bashrc
 # mamba activate snakemake
 
 # run snakemake for whole pipeline (ending with volcano plot)
-snakemake -c 4 --use-conda --jobs 32 --cluster 'bsub -W 48:00 -n 8 -R "rusage[mem=16G]" -o out.%J.txt -e err.%J.txt' results/SCENT_peak_gene/significant_peak_gene_associations.csv
+snakemake -c 4 --use-conda --jobs 32 --cluster 'SLURM -W 48:00 -n 8 -R "rusage[mem=16G]" -o out.%J.txt -e err.%J.txt' results/SCENT_peak_gene/significant_peak_gene_associations.csv
 
 # Random comments pertaining to various components of the job submission.
 # .%J adds job ID number to output files
-# run using bsub < run_snakemake.sh. '<'
+# run using SLURM < run_snakemake.sh. '<'
