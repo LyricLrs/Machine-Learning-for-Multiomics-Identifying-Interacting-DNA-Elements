c0df53fc08b0c296bb0880a4b053f8647b2e5c6c
Anni Zheng <annizheng3@gmail.com>
2024-10-10 23:03:45 -0400

    Update run_snakemake.sh

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -16,6 +16,8 @@ source $HOME/.bashrc
 # HPC has its own snakemake so we would use that
 # mamba activate snakemake
 
+snakemake --unlock
+
 # run snakemake for whole pipeline (ending with volcano plot)
 # snakemake --use-conda --jobs 32 --cluster 'bsub -W 48:00 -n 8 -R "rusage[mem=16G]" -o out.%J.txt -e err.%J.txt' results/SCENT_peak_gene/significant_peak_gene_associations.csv
 snakemake --use-conda --jobs 32 --cluster 'sbatch --time=48:00:00 --cpus-per-task=8 --mem=16G -o out.%J.txt -e err.%J.txt' results/SCENT_peak_gene/significant_peak_gene_associations.csv
