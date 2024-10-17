2631d47f9ede6dbba8de8da40b3a012417017055
Anni Zheng <annizheng3@gmail.com>
2024-10-17 16:55:04 -0400

    update input data

--- data/data_seg.r
+++ data/data_seg.r
@@ -4,8 +4,8 @@ atac_matrix <- readRDS("data/PBMC/atac_matrix.rds")
 rna_matrix <- readRDS("data/PBMC/rna_matrix.rds")
 metafile <- readRDS("data/PBMC/metafile.rds")
 
-atac_matrix_100 <- atac_matrix[1:100, 1:100]
-rna_matrix_100 <- rna_matrix[1:100, 1:100]
+atac_matrix_100 <- atac_matrix[1:100, ]
+rna_matrix_100 <- rna_matrix[1:100, ]
 
 saveRDS(atac_matrix_100, "data/atac_matrix.rds")
 saveRDS(rna_matrix_100, "data/rna_matrix.rds")
