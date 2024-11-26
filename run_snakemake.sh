348805504afac08f385a8cafe9389b7a9d55d81d
Anni Zheng <annizheng3@gmail.com>
2024-11-26 15:44:46 -0500

    Update run_snakemake.sh

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -20,6 +20,6 @@ export CONDA_PKGS_DIRS=conda_cache/pkgs
 snakemake --unlock
 
 # Run Snakemake with Conda, specifying your local Conda cache and cluster submission settings
-snakemake --use-conda --conda-prefix $HOME/conda_cache/envs \
+snakemake --use-conda --conda-prefix conda_cache/envs \
     --jobs 32 \
     --cluster 'bsub -W 48:00 -n 8 -R "rusage[mem=16G]" -o out.%J.txt -e err.%J.txt'
