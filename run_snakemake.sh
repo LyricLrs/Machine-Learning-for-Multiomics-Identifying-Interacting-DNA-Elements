0608bdee785165c914c1a414ba8573cc9a462059
Anni Zheng <annizheng3@gmail.com>
2024-11-26 15:14:09 -0500

    One more update

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -21,12 +21,10 @@ conda install -c conda-forge r-roxygen2 -y
 # activate snakemake mamba environment
 # conda activate snakemake
 
-# Prevent Lock Error
 snakemake --unlock
-
-# run snakemake for whole pipeline (ending with volcano plot)
+snakemake --use-conda --jobs 32 --cluster 'bsub -W 48:00 -n 8 -R "rusage[mem=16G]" -o out.%J.txt -e err.%J.txt'
 # snakemake --use-conda --jobs 32 --cluster 'bsub -W 48:00 -n 8 -R "rusage[mem=16G]" -o out.%J.txt -e err.%J.txt' results/SCENT_peak_gene/significant_peak_gene_associations.csv
-snakemake --use-conda --jobs 32 --cluster 'sbatch --time=48:00:00 --cpus-per-task=1 --mem=16G -o out.%J.txt -e err.%J.txt' $(for i in {1..32}; do echo "results/epistasis_models/epistasis_models_01_10_${i}.csv"; done)
+# snakemake --use-conda --jobs 32 --cluster 'sbatch --time=48:00:00 --cpus-per-task=1 --mem=16G -o out.%J.txt -e err.%J.txt' $(for i in {1..32}; do echo "results/epistasis_models/epistasis_models_01_10_${i}.csv"; done)
 
 # Random comments pertaining to various components of the job submission.
 # .%J adds job ID number to output files
